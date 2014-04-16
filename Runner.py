bc9c571cf627a63e635eaf9c21942401236361ad
princeladdak <princeladdak@gmail.com>
2014-04-16 22:48:42 +0530

    Updates now it also prints the most similar User

--- Runner.py
+++ Runner.py
@@ -4,15 +4,29 @@ from Reccomendations import critics
 
 #the following routing take each person and finds out its similarity with other person
 
+max=0.0
+m_sim=" "
+
 
 for item in critics:
+    max=0.0
+    m_sim=" "
     for item1 in critics:
         if item != item1:
+            var=Reccomendations.sim_distance(Reccomendations.critics,item,item1)
+            if var > max :
+                max=var
+                m_sim=item1
+
             print "Similarity Between"
             print ("Person 1:-",item)
             print ("Person 2:-",item1)
-            print Reccomendations.sim_distance(Reccomendations.critics,item,item1)
+            print  var
             print "\n"
 
 
+    print ("for "+item+ " ")
+    print ("The greatest similarity is found with "+m_sim)
+    print ("Which is equal to:",max)
+    print ("\n\n")
 
